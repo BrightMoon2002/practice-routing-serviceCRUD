c44a1d2894ba459bdbce77a6f33a173d74d0f99e
nhimdom200292 <kiemda02@gmail.com>
2022-01-06 10:34:51 +0700

    // api local and test api serve ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   //API_LOCAL
-  API_LOCAL: 'http://localhost:8080/api/books',
+  // API_LOCAL: 'http://localhost:8080/api/books',
   //API_HEROKU
   API_SERVER: 'https://bookrs2.herokuapp.com/api/books',
   production: true
